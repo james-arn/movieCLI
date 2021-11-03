e93b579e929abe8f04c8a10017c7e30ae4a8daf0
James Arnold <j1mesu@hotmail.co.uk>
2021-11-03 12:12:00 +0000

    final update

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,22 +1,61 @@
-const fs = require("fs");
+const yargs = require("yargs");
 
-const addMovie = (movieObj) => {
+const addMovie = async (collection, dataObj) => {
   try {
-    const stringyObj = JSON.stringify(movieObj); //converts obj to string
-    fs.writeFileSync("./storage.json", stringyObj); // creates file if doesn't exist
+    await collection.insertOne(dataObj);
+    console.log("Successfully added movie");
   } catch (error) {
     console.log(error);
   }
 };
 
-const listMovies = () => {
+const findResult = async (collection, dataObj) => {
   try {
-    const jsonList = fs.readFileSync("./storage.json"); //creates list and reads the list.
-    const list = JSON.parse(jsonList);
-    console.log(list);
+    const list = await collection.find(dataObj).toArray();
+    console.log("Found documents =>", list);
   } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = { addMovie, listMovies };
+const filterFindRating = async (collection, dataObj) => {
+  try {
+    const filteredDocs = await collection
+      .find({ rating: dataObj.rating })
+      .toArray();
+    console.log(`Found films filtered by ${dataObj.rating} =>`, filteredDocs);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateActor = async (collection, dataObj) => {
+  try {
+    await collection.updateOne(
+      { title: dataObj.title },
+      { $set: { actor: dataObj.actor } }
+    );
+    console.log(
+      `Successfully updated ${dataObj.title} actor to ${dataObj.actor}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteResult = async (collection, dataObj) => {
+  try {
+    const deleteData = await collection.deleteMany({ title: dataObj.title });
+    console.log("Deleted documents =>", deleteData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  addMovie,
+  findResult,
+  updateActor,
+  deleteResult,
+  filterFindRating,
+};
